4a0fade788562787116d1325a8a0e554ba13fa4f
Rolando Cruz <pspnando@me.com>
2015-05-23 17:17:59 -0700

    pushed app

---
+++ OcrSdkDemo/Sources/RecognitionViewController.m
@@ -0,0 +1,130 @@
+#import "RecognitionViewController.h"
+#import "AppDelegate.h"
+
+//#error Provide Application ID and Password
+// To create an application and obtain a password,
+// register at http://cloud.ocrsdk.com/Account/Register
+// More info on getting your application id and password at
+// http://ocrsdk.com/documentation/faq/#faq3
+
+// Name of application you created
+static NSString* MyApplicationID = @"testHack"; //testAppMay22
+// Password should be sent to your e-mail after application was created
+static NSString* MyPassword = @"fzs1XmPvy+p50vRP6lIiNAAb"; //LbETIGHLIUCENqys8McBQbhs
+
+@implementation RecognitionViewController
+
+@synthesize textView;
+@synthesize statusLabel;
+@synthesize statusIndicator;
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+	[self setTextView:nil];
+	[self setStatusLabel:nil];
+	[self setStatusIndicator:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	textView.hidden = YES;
+	
+	statusLabel.hidden = NO;
+	statusIndicator. hidden = NO;
+	
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	statusLabel.text = @"Loading image...";
+	
+	UIImage* image = [(AppDelegate*)[[UIApplication sharedApplication] delegate] imageToProcess];
+	
+	Client *client = [[Client alloc] initWithApplicationID:MyApplicationID password:MyPassword];
+	[client setDelegate:self];
+	
+	if([[NSUserDefaults standardUserDefaults] stringForKey:@"installationID"] == nil) {
+		NSString* deviceID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+		
+		NSLog(@"First run: obtaining installation ID..");
+		NSString* installationID = [client activateNewInstallation:deviceID];
+		NSLog(@"Done. Installation ID is \"%@\"", installationID);
+		
+		[[NSUserDefaults standardUserDefaults] setValue:installationID forKey:@"installationID"];
+	}
+	
+	NSString* installationID = [[NSUserDefaults standardUserDefaults] stringForKey:@"installationID"];
+	
+	client.applicationID = [client.applicationID stringByAppendingString:installationID];
+	
+	ProcessingParams* params = [[ProcessingParams alloc] init];
+	
+	[client processImage:image withParams:params];
+	
+	statusLabel.text = @"Uploading image...";
+	
+    [super viewDidAppear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return NO;
+}
+
+#pragma mark - ClientDelegate implementation
+
+- (void)clientDidFinishUpload:(Client *)sender
+{
+	statusLabel.text = @"Processing image...";
+}
+
+- (void)clientDidFinishProcessing:(Client *)sender
+{
+	statusLabel.text = @"Downloading result...";
+}
+
+- (void)client:(Client *)sender didFinishDownloadData:(NSData *)downloadedData
+{
+	statusLabel.hidden = YES;
+	statusIndicator.hidden = YES;
+	
+	textView.hidden = NO;
+	
+	NSString* result = [[NSString alloc] initWithData:downloadedData encoding:NSUTF8StringEncoding];
+	
+	textView.text = result; 
+}
+
+- (void)client:(Client *)sender didFailedWithError:(NSError *)error
+{
+	UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Error"
+													message:[error localizedDescription]
+												   delegate:nil 
+										  cancelButtonTitle:@"Cancel" 
+										  otherButtonTitles:nil, nil];
+	
+	[alert show];
+	
+	statusLabel.text = [error localizedDescription];
+	statusIndicator.hidden = YES;
+}
+
+@end
